d48731fa67c8fc1a5654f885b8abc597e6b9f712
Gautam Borkar <gborkar@us.ibm.com>
2021-06-28 14:03:04 -0400

    Added sample NodeJS express app

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,26 @@ import * as path from 'path';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	const tutorialUri = vscode.Uri.file(path.join(__filename, '..', '..') + "/README.md");
-	await vscode.commands.executeCommand('vscode.didact.startDidact', tutorialUri);
+    const activated = await activateDidactExtension();
+    if (!activated) {
+        return;
+    }
+
+    await vscode.commands.executeCommand('workbench.action.closeAllEditors');
+    const tutorialUri = vscode.Uri.file(path.join(__filename, '..', '..') + "/README.md");
+    await vscode.commands.executeCommand('vscode.didact.startDidact', tutorialUri);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
+
+// This method activates the didact extension
+async function activateDidactExtension(): Promise<boolean> {
+    const extensions = vscode.extensions.all;
+    const didactExt = extensions.find(ext => ext.id === 'redhat.vscode-didact');
+    if (didactExt) {
+        await didactExt?.activate();
+        return true;
+    }
+    return false;
+}
